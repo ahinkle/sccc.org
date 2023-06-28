b5c885c2073a8fa83a674acc5e204013c81eb31e
Andy Hinkle <ahinkle10@gmail.com>
2023-06-28 15:54:22 -0500

    Add Landing Page

---
+++ resources/views/coming-soon.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Coming Soon | Santa Claus Christian Church</title>
+
+        <script src="https://cdn.tailwindcss.com"></script>
+
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,300;0,500;1,400&display=swap" rel="stylesheet">
+
+        <style>
+            html {
+                background: url(img/background/SCCC_HighAngle-min.jpg) no-repeat center center fixed;
+                -webkit-background-size: cover;
+                -moz-background-size: cover;
+                -o-background-size: cover;
+                background-size: cover;
+                font-family: 'Roboto', sans-serif !important;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="absolute top-0 left-0 w-full h-full bg-gray-900 opacity-75">
+            <div class="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 text-white font-bold text-center w-full px-4">
+                <span class="text-xl text-white pl-2 uppercase">Santa Claus Christian Church</span>
+                <span class="text-white pl-2 block italic">Santa Claus, Indiana</span>
+                <h1 class="text-3xl md:text-5xl pt-5 uppercase">Website Coming Soon</h1>
+
+                <h2 class="pt-5 uppercase">Worship with us:</h2>
+
+                <div class="pt-2">
+                    <p>Sunday Worship: 9:00 am</p>
+                    <p>Wednesday (youth): 6:00 pm</p>
+                </div>
+
+                <div class="pt-10">
+                    <a href="https://youtube.com/@SantaClausChristianChurch" target="_blank" class="text-2xl text-white hover:text-red-500 underline">Watch on YouTube</a>
+
+                    <div class="pt-5">
+                        <a href="https://www.facebook.com/SantaClausChristianChurch" target="_blank" class="text-base text-white hover:text-blue-500 underline">Facebook</a>
+                        <span class="text-white px-2">&#x2022;</span>
+                        <a href="https://www.instagram.com/santaclauschristianchurch/" target="_blank" class="text-base text-white hover:text-blue-500 underline">Instagram</a>
+                    </div>
+                </div>
+
+                <div class="pt-10 mx-auto max-w-sm">
+                    <a href="https://cfcnewbedford.org/" target="_blank" class="text-sm text-white hover:text-blue-500 underline">looking for South Coast Community Church (former sccc.org), now Christian Fellowship Center in New Bedford, Massachusetts?</a>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
