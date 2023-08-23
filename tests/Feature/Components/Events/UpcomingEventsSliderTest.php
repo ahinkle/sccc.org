e16b679e7b97fd770d61519f2ee5fbaabe4bdc9d
Andy Hinkle <ahinkle10@gmail.com>
2023-08-23 09:42:49 -0500

    Initall / Configure Pest

--- tests/Feature/Components/Events/UpcomingEventsSliderTest.php
+++ tests/Feature/Components/Events/UpcomingEventsSliderTest.php
@@ -1,27 +1,19 @@
 <?php
 
-namespace Tests\Feature\Components\Events;
-
 use App\Models\Event;
-use Tests\TestCase;
 
-class UpcomingEventsSliderTest extends TestCase
-{
-    public function test_it_shows_upcoming_event(): void
-    {
-        $e = Event::factory()->upcoming()->create();
+test('it shows upcoming event', function () {
+    $e = Event::factory()->upcoming()->create();
 
-        $component = $this->blade('<x-events.upcoming-events-slider />');
+    $component = $this->blade('<x-events.upcoming-events-slider />');
 
-        $component->assertSee($e->name);
-    }
+    $component->assertSee($e->name);
+});
 
-    public function test_it_does_not_show_past_event(): void
-    {
-        $e = Event::factory()->past()->create();
+test('it does not show past event', function () {
+    $e = Event::factory()->past()->create();
 
-        $component = $this->blade('<x-events.upcoming-events-slider />');
+    $component = $this->blade('<x-events.upcoming-events-slider />');
 
-        $component->assertDontSee($e->name);
-    }
-}
+    $component->assertDontSee($e->name);
+});
--- tests/Feature/Pages/HomepageTest.php
+++ tests/Feature/Pages/HomepageTest.php
@@ -1,15 +1,7 @@
 <?php
 
-namespace Tests\Feature\Pages;
-
-use Tests\TestCase;
-
-class HomepageTest extends TestCase
-{
-    public function test_it_renders_homepage(): void
-    {
-        $this->get('/')
-            ->assertOk()
-            ->assertSee('Santa Claus Christian Church');
-    }
-}
+test('it renders homepage', function () {
+    $this->get('/')
+        ->assertOk()
+        ->assertSee('Santa Claus Christian Church');
+});
