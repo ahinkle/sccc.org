982301684f0fb19e8989cee2a4c58307ba0474d6
Andy Hinkle <ahinkle10@gmail.com>
2023-08-26 09:22:29 -0500

    Ability to create events in Filament

--- app/Filament/Resources/EventResource.php
+++ app/Filament/Resources/EventResource.php
@@ -2,12 +2,15 @@
 
 namespace App\Filament\Resources;
 
-use App\Filament\Resources\EventResource\Pages;
+use Filament\Forms;
+use App\Enums\State;
+use Filament\Tables;
 use App\Models\Event;
 use Filament\Forms\Form;
-use Filament\Resources\Resource;
-use Filament\Tables;
 use Filament\Tables\Table;
+use App\Enums\EventFrequency;
+use Filament\Resources\Resource;
+use App\Filament\Resources\EventResource\Pages;
 
 class EventResource extends Resource
 {
@@ -19,7 +22,73 @@ class EventResource extends Resource
     {
         return $form
             ->schema([
-                //
+                Forms\Components\Fieldset::make('Event Details')
+                    ->schema([
+                        Forms\Components\TextInput::make('name')
+                            ->autofocus()
+                            ->required()
+                            ->label('Event Name'),
+                        Forms\Components\FileUpload::make('image')
+                            ->required()
+                            ->label('Event Image')
+                            ->image()
+                            ->imageEditor(),
+                        Forms\Components\Textarea::make('description')
+                            ->required()
+                            ->label('Event Description')
+                            ->columnSpan(2),
+                    ]),
+
+                Forms\Components\Fieldset::make('Event Time')
+                    ->schema([
+                        Forms\Components\DateTimePicker::make('starts_at')
+                            ->required()
+                            ->seconds(false)
+                            ->label('Event Start')
+                            ->afterOrEqual('today'),
+                        Forms\Components\DateTimePicker::make('ends_at')
+                            ->label('Event End')
+                            ->seconds(false)
+                            ->helperText('Optional. Add only if you want to specify an end time on the event page.')
+                            ->nullable()
+                            ->afterOrEqual('starts_at'),
+                        Forms\Components\Select::make('repeat_frequency')
+                            ->label('Recurring Schedule')
+                            ->options(EventFrequency::class)
+                            ->placeholder('Event does not repeat')
+                            ->helperText('Automatically creates recurring events on the event page. For example, if you select "Weekly", the event will be created on the event page every week. Use the "End Date" field to specify when the recurring events should stop being created.')
+                    ]),
+
+                 Forms\Components\Fieldset::make('Event Location')
+                    ->schema([
+                        Forms\Components\TextInput::make('location')
+                            ->required()
+                            ->label('Event Location')
+                            ->default('Santa Claus Christian Church')
+                            ->columnSpan(2),
+                        Forms\Components\TextInput::make('address')
+                            ->required()
+                            ->label('Event Address')
+                            ->default('351 N Holiday Blvd')
+                            ->columnSpan(2),
+                        Forms\Components\Grid::make()
+                            ->columns(3)
+                            ->schema([
+                                Forms\Components\TextInput::make('city')
+                                    ->required()
+                                    ->label('Event City')
+                                    ->default('Santa Claus'),
+                                Forms\Components\Select::make('state')
+                                    ->required()
+                                    ->label('Event State')
+                                    ->options(State::class)
+                                    ->default(State::INDIANA),
+                                Forms\Components\TextInput::make('zip_code')
+                                    ->required()
+                                    ->label('Event Zip Code')
+                                    ->default('47579'),
+                                ])
+                    ]),
             ]);
     }
 
@@ -35,6 +104,10 @@ class EventResource extends Resource
                     ->label('Event Start')
                     ->dateTime('M j, Y g:i A')
                     ->sortable(),
+
+                Tables\Columns\TextColumn::make('ends_at')
+                    ->label('Event End')
+                    ->dateTime('M j, Y g:i A'),
             ])
             ->filters([
                 //
