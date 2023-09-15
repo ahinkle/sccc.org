df43558ef4dd9b88b2f325197188261cacc9c4a7
Andy Hinkle <ahinkle10@gmail.com>
2023-09-15 14:57:14 -0500

    Decrease Background Brightness

--- resources/views/components/hero/page-hero.blade.php
+++ resources/views/components/hero/page-hero.blade.php
@@ -1,4 +1,4 @@
-<div class="w-full bg-cover bg-center h-[450px]" style="background-image: url({{ $bgUrl }})">
+<div class="w-full bg-cover bg-center h-[450px]" style="background-image: url({{ $bgUrl }}); background-color: rgba(0, 0, 0, 0.50); background-blend-mode: multiply;">
     <div class="max-w-screen-xl mx-auto flex items-center h-full px-5 xl:px-0 place-content-center lg:place-content-start">
         {{ $slot }}
     </div>
