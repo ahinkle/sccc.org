70f5a4f76c810cb9b1e715b7cbb741a4449b9461
Andy Hinkle <ahinkle10@gmail.com>
2023-09-20 07:42:43 -0500

    Command to republish recurring events manually

---
+++ app/Console/Commands/Events/PublishRecurringEventsCommand.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\Console\Commands\Events;
+
+use App\Jobs\Events\PublishRecurringEvents;
+use Illuminate\Console\Command;
+
+class PublishRecurringEventsCommand extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'events:recurring {--force}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Publish recurring events from the prior day.';
+
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        if ($this->option('force') || $this->confirm('This will launch a background task to publish recurring events from the prior day. Are you sure you want to continue?', true)) {
+            $this->info('Publishing recurring events from the prior day...');
+            dispatch(new PublishRecurringEvents());
+            $this->info('Successfully launched task.');
+        }
+    }
+}
---
+++ tests/Feature/App/Console/Commands/Events/PublishgRecurringEventsCommandTest.php
@@ -0,0 +1,16 @@
+<?php
+
+use App\Jobs\Events\PublishRecurringEvents;
+use Illuminate\Support\Facades\Queue;
+
+it('queues job from command execution', function () {
+    Queue::fake();
+
+    $this->artisan('events:recurring')
+        ->expectsConfirmation('This will launch a background task to publish recurring events from the prior day. Are you sure you want to continue?', 'yes')
+        ->expectsOutput('Publishing recurring events from the prior day...')
+        ->expectsOutput('Successfully launched task.')
+        ->assertExitCode(0);
+
+    Queue::assertPushed(PublishRecurringEvents::class);
+});
