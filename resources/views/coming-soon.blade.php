2fe833edf07ed564b61628b6d78c59e1f3817004
Andy Hinkle <ahinkle10@gmail.com>
2023-09-23 20:59:21 -0500

    Update homepage

--- resources/views/coming-soon.blade.php
+++ resources/views/coming-soon.blade.php
@@ -37,10 +37,6 @@
                     <p>Wednesday (youth): 6:00 pm</p>
                 </div>
 
-                <div class="pt-2">
-                    <a href="https://tinyurl.com/SCCCWomen" target="_blank" class="text-xl text-white hover:text-red-500 underline">Click here to register for Womens' Conference</a>
-                </div>
-
                 <div class="pt-6">
                     <a href="https://youtube.com/@SantaClausChristianChurch" target="_blank" class="text-2xl text-white hover:text-red-500 underline">Watch on YouTube</a>
 
