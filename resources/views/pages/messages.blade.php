fdbad8274009191fb7721e59a1e3e144a24c5be6
Andy Hinkle <ahinkle10@gmail.com>
2023-10-07 21:37:56 -0500

    Fix image paths

--- resources/views/pages/messages.blade.php
+++ resources/views/pages/messages.blade.php
@@ -4,7 +4,7 @@
         <meta name="description" content="View our past messages and sermons from Santa Claus Christian Church.">
     </x-slot>
 
-    <x-hero.page-hero bgUrl="{{ url('/img/headers/messages.jpg') }}">
+    <x-hero.page-hero bgUrl="{{ url('/img/headers/messages.JPG') }}">
        <h1 class="text-5xl font-bold text-white font-sen border-b-4 pb-2 uppercase">Messages</h1>
     </x-hero.page-hero>
 
