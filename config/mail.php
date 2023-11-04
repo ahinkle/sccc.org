b75b4010701d54c2754a87116bcb1c05cc9017bf
Andy Hinkle <ahinkle10@gmail.com>
2023-11-03 22:36:18 -0500

    Adds AWS SES SDK

--- config/mail.php
+++ config/mail.php
@@ -48,6 +48,8 @@ return [
 
         'ses' => [
             'transport' => 'ses',
+            'secret' => env('AWS_SECRET_ACCESS_KEY'),
+            'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
         ],
 
         'mailgun' => [
