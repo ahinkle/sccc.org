d724714a065b19a1fad70fdf2fc651d1c57c337b
ahinkle <ahinkle@grayloon.com>
2024-01-21 17:03:59 -0600

    Move from Current Series to overview of worship on home page

--- resources/views/components/cards/current-message-series.blade.php
+++ resources/views/components/cards/current-message-series.blade.php
@@ -1,11 +1,14 @@
 <x-cards.two-column-card>
     <div>
-        <img src="{{ asset('img/series/living-out-gods-love.jpg') }}" alt="Current Message Series" class="w-full h-full object-cover max-w-lg xl:max-w-none xl:mx-auto">
+        <img src="{{ asset('img/grounded/message.JPG') }}" alt="Grounded: Traditonal Worship" class="w-full h-full object-cover max-w-lg xl:max-w-none xl:mx-auto">
     </div>
     <div class="xl:px-10 self-center grid gap-y-10">
-        <h2 class="text-4xl font-poppins font-semibold uppercase">Living Out God's Love</h2>
-        <h3 class="text-xl font-poppins font-semibold uppercase">Current Message Series</h3>
-        <p class="text-base font-poppins">Discover how to cultivate compassion, kindness, and empathy as we navigate the challenges of our modern world. Through thought-provoking messages, heartfelt stories, and practical guidance, we aim to inspire you to deepen your connection with God's love and share it with others.</p>
+        <h2 class="text-4xl font-poppins font-semibold uppercase">GROUNDED: Traditional Worship</h2>
+        <h3 class="text-xl font-poppins font-semibold uppercase">Sunday 9:00 AM</h3>
+        <p class="text-base font-poppins">
+            We extend a warm invitation to join us at Santa Claus Christian Church! Whether you're a long-time believer or just curious to learn more about Jesus Christ, we welcome you to our traditonal worship service where we sing hymns, pray, and hear a message from our pastor.
+            Come experience the love and teachings of Jesus Christ with us. Our doors are open, and we look forward to sharing our faith journey with you.
+        </p>
 
         <x-inputs.button href="{{ route('messages.latest' )}}" target="_blank">Watch the latest message</x-inputs.button>
     </div>
