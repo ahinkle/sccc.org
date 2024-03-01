314b42fedcaab62bad0f776a438147aee60ed4f6
ahinkle <ahinkle@grayloon.com>
2024-02-29 21:18:58 -0600

    add global top bar

---
+++ app/View/Components/Layout/GlobalTopBar.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\View\Components\Layout;
+
+use Ahinkle\AutoResolvableComponents\AutoResolvableComponent;
+
+class GlobalTopBar extends AutoResolvableComponent
+{
+    //
+}
---
+++ resources/views/components/layout/global-top-bar.blade.php
@@ -0,0 +1,6 @@
+<div class="flex flex-wrap px-5 bg-green-700 text-white text-center py-2">
+    <p class="w-full text-lg font-sans">
+        Elementary Jedi Camp 2024: 
+        <a href="{{ route('events.jedi-camp') }}" class="underline">Registration now open</a>
+    </p>
+</div>
