a6a4f15212d4d789b38da02064c2ed1e78453dca
ahinkle <ahinkle@grayloon.com>
2024-02-29 21:19:44 -0600

    Adjust fonts

--- resources/views/components/layout/global-top-bar.blade.php
+++ resources/views/components/layout/global-top-bar.blade.php
@@ -1,5 +1,5 @@
 <div class="flex flex-wrap px-5 bg-green-700 text-white text-center py-2">
-    <p class="w-full text-lg font-sans">
+    <p class="w-full text-sm lg:text-base font-sans">
         Elementary Jedi Camp 2024: 
         <a href="{{ route('events.jedi-camp') }}" class="underline">Registration now open</a>
     </p>
