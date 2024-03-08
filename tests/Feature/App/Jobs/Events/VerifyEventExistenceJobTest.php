739192c74d597f3fd5a3ac2faacbdc68adb385af
ahinkle <ahinkle@grayloon.com>
2024-03-07 21:31:11 -0600

    wip

--- tests/Feature/App/Jobs/Events/VerifyEventExistenceJobTest.php
+++ tests/Feature/App/Jobs/Events/VerifyEventExistenceJobTest.php
@@ -1,13 +1,13 @@
 <?php
 
-use App\Models\Event;
-use Illuminate\Support\Facades\Http;
-use Illuminate\Support\Facades\Cache;
 use App\Jobs\Events\VerifyEventExistenceJob;
+use App\Models\Event;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Http;
 
 it('removes event that is no longer in elexio', function () {
     Cache::put('elexio_session_id', '1234567890', 60 * 10);
-    
+
     $event = Event::factory()->create([
         'elexio_id' => 123,
     ]);
@@ -23,7 +23,7 @@ it('removes event that is no longer in elexio', function () {
 
 it('keeps event that is still in elexio', function () {
     Cache::put('elexio_session_id', '1234567890', 60 * 10);
-    
+
     $event = Event::factory()->create([
         'elexio_id' => 123,
     ]);
