5185dad68741f779d99a985166255741325dac9a
ahinkle <ahinkle@grayloon.com>
2024-03-08 11:07:05 -0600

    chore: add wrw redirect

--- routes/web.php
+++ routes/web.php
@@ -18,9 +18,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 /** Temporary Pages */
-Route::redirect('/events/silentretreat', 'https://www.elexiogiving.com/App/Form/43503380-4509-4be6-9488-64f4fa49e644')->name('events.silentretreat');
-Route::view('/jedicamp', 'pages.events.jedi-camp-2024')->name('events.jedi-camp');
-/** End of Temporary Pages */
+Route::redirect('/events/womensconference', 'https://www.elexiogiving.com/App/Form/2c3c9dd2-90a4-4b71-b550-05ad1c38d3a2')->name('events.womensconference'); /* Remove after Apr 20, 2024 */
+Route::redirect('/events/silentretreat', 'https://www.elexiogiving.com/App/Form/43503380-4509-4be6-9488-64f4fa49e644')->name('events.silentretreat'); /* Remove after Mar 14, 2024 */
+Route::view('/jedicamp', 'pages.events.jedi-camp-2024')->name('events.jedi-camp'); /* Remove after Jul 12, 2024 */
+
 Route::view('/', 'pages.home')->name('home');
 
 Route::view('/contact-us', 'pages.contact-us')->name('contact-us');
