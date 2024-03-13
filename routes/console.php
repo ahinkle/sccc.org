50e72d29cf81d8b7f88624ba905bfa70fe6d886c
ahinkle <ahinkle@grayloon.com>
2024-03-12 20:47:34 -0500

    wip

--- routes/console.php
+++ routes/console.php
@@ -4,15 +4,14 @@ use App\Jobs\Events\PublishEventsFromFeedJob;
 use App\Jobs\Livestream\PublishLivestreamAsMessageJob;
 use App\Jobs\Livestream\UpdateUpcomingLivestreamJob;
 use App\Jobs\NewsletterContacts\PruneUnverifiedNewsletterContactsJob;
-use Illuminate\Support\Facades\Artisan;
 use Illuminate\Support\Facades\Schedule;
 
-Artisan::command('queue:prune-batches')->daily();
-Artisan::command('events:purge')->everySixHours();
-Artisan::command('events:verify')->everySixHours();
+Schedule::command('queue:prune-batches')->daily();
+Schedule::command('events:purge')->everySixHours();
+Schedule::command('events:verify')->everySixHours();
 
-Schedule::call(new PublishEventsFromFeedJob(today(), today()->addWeek()->endOfDay()))->hourly();
-Schedule::call(new PublishEventsFromFeedJob(today()->addDays(8), today()->addMonths(6)->endOfDay()))->everySixHours();
-Schedule::call(new PruneUnverifiedNewsletterContactsJob())->daily();
-Schedule::call(new UpdateUpcomingLivestreamJob())->weeklyOn(6, '9:00');
-Schedule::call(new PublishLivestreamAsMessageJob())->weeklyOn(0, '11:00');
+Schedule::job(new PublishEventsFromFeedJob(today(), today()->addWeek()->endOfDay()))->hourly();
+Schedule::job(new PublishEventsFromFeedJob(today()->addDays(8), today()->addMonths(6)->endOfDay()))->everySixHours();
+Schedule::job(new PruneUnverifiedNewsletterContactsJob())->daily();
+Schedule::job(new UpdateUpcomingLivestreamJob())->weeklyOn(6, '9:00');
+Schedule::job(new PublishLivestreamAsMessageJob())->weeklyOn(0, '11:00');
